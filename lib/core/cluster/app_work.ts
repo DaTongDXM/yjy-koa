44ef8957a851b2e124b85921f77b8414ee1168be
wuxudong <wuxudong@fang.com>
2023-03-14 17:11:19 +0800

    ✨ feat: 多进程app_worker模块

---
+++ lib/core/cluster/app_work.ts
@@ -0,0 +1,8 @@
+
+import ConsoleLog from "../utils/consoleLog";
+import YjyKoa from '../../core/index';
+
+const options = JSON.parse(process.argv[2]);
+const log = new ConsoleLog();
+// const app = new YjyKoa(options)
+// console.log(app)
